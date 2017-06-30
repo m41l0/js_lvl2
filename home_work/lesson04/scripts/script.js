2465c44e8faa75358bb7d81be6353a828b77d45b
Maksim Loginov <loginovmi@gmail.com>
2017-06-30 22:35:14 +1000

    complete lesson04/task01 home work

--- home_work/lesson04/scripts/script.js
+++ home_work/lesson04/scripts/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     $('.tabs .tab-links a').click(function(event)  {
         var currentAttrValue = $(this).attr('href');
-        console.log(currentAttrValue);
-        // Show/Hide Tabs
-        $('.tabs ' + currentAttrValue).fadeIn(500).siblings().hide();
-        // Change/remove current tab to active
+
+        // скрываем вкладки, кроме остальных
+        $('.tabs ' + currentAttrValue).fadeIn(1000).siblings().hide();
+
+        // добавляем/удаляем для нужного пункта меню класс active
         $(this).parent('li').addClass('active').siblings().removeClass('active');
         event.preventDefault();
     });
